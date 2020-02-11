857a1314a84617a2f79d6d4fd85dbb434036a8c0
ktbsh3 <jan.samoel@gmail.com>
2020-02-11 15:03:13 +0100

    laying the groundwork for migrating fylesystem operations into a single class

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var projpath = '/home/ktbsh/werk/telka/node_web/kyoma';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { obrazky: fs.readdirSync(projpath+'/public/images'), slidespeed: jsonfile.readFileSync(projpath+'/data.json').speed });
+  res.render('index', { obrazky: fs.readdirSync(req.app.locals.basedir+'/public/images'), slidespeed: jsonfile.readFileSync(projpath+'/data.json').speed });
 });
 
 module.exports = router;
